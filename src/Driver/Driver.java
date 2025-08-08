5bf64ea80d63cc4f26416fe08befca1b9a2ef12d
Winese <wineselow@gmail.com>
2025-08-08 19:54:57 +0800

    Add files via upload

---
+++ src/Driver/Driver.java
@@ -0,0 +1,35 @@
+import Commands.*;
+import Tools.Invoker;
+import Tools.Receiver;
+import java.util.Stack;
+
+/**
+ * Create and configure the command objects
+*/
+public class Driver {
+
+    public static void main(String[] args) {
+        testCase();
+        //testCase();
+    }
+
+    public static void testCase() {
+        Stack<Command> history = new Stack<Command>();
+        Receiver receiver = new Receiver();
+
+        Command[] cmdlist = {
+                new AddCommand(receiver,"1 %2FTnDD t3_4@edd.com"),
+                new AddCommand(receiver,"First_name Last_name email"),
+                new UpdateCommand(receiver,"1 2"),
+                new UndoCommand(receiver, history),
+                new DeleteCommand(receiver,"999"),
+                new UndoCommand(receiver, history),
+                new ListCommand(receiver)
+        };
+        Invoker invoker = new Invoker();
+        invoker.setCommandsForExecution(cmdlist);
+        invoker.executeCommand(history);
+
+        receiver.storeToFile();
+    }
+}
